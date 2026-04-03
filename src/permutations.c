5e66f5854c6322718f2b3934e27d12a95968bb4b
babidrummond <babidrummond@hotmail.com>
2026-04-03 01:16:09 -0300

    feat: optimized permutations

--- src/permutations.c
+++ src/permutations.c
@@ -6,7 +6,7 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 20:02:10 by bmoreira          #+#    #+#             */
-/*   Updated: 2026/04/03 00:43:50 by bmoreira         ###   ########.fr       */
+/*   Updated: 2026/04/03 01:15:40 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,33 +55,61 @@ int	has_duplicate(char *str, char c)
 	return (0);
 }
 
-void	permutations(char *set, char *string, int pos)
+void order_str(char *string, char *ascii)
 {
+	char *new;
 	int i;
 
 	i = 0;
+	while (string[i])
+	{
+		ascii[(int)string[i]] = 1;
+		i++;
+	}
+	i = 0;
+	new = ascii;
+	while (i < 256)
+	{
+		if (ascii[i])
+			*new++ = i;
+		i++;
+	}
+	*new = 0;
+}
+
+void	permutations(char *set, char *used, char *string, int pos)
+{
+	int i;
+
+	i = -1;
 	if (!set[pos])
 	{
 		printf("%s\n", string);
 		return ;
 	}
-	while (set[i])
+	while (set[++i])
 	{
+		if (used[(int) set[i]])
+			continue ;
+		used[(int) set[i]] = 1;
 		string[pos] = set[i];
-		if (!has_duplicate(string, string[pos]))
-			permutations(set, string, pos + 1);
-		string[pos] = 0;
-		i++;
+		permutations(set, used, string, pos + 1);
+		used[(int) set[i]] = 0;
 	}
 }
 
 int main(int argc, char **argv)
 {	
+	char ordered[256] = {0};
+	char used[256] = {0};
 	char *new;
+	char *dup;
 
 	if (argc != 2)
 		return (1);
 	new = calloc(ft_strlen(argv[1]) + 1, sizeof(char));
-	permutations(ft_strdup(argv[1]), new, 0);
+	dup = ft_strdup(argv[1]);
+	order_str(dup, ordered);
+	permutations(ordered, used, new, 0);
 	return (0);
 }
