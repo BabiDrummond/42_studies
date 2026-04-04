4d81cdea11e3ae787a8757e17c5652044c244471
babidrummond <babidrummond@hotmail.com>
2026-04-03 21:47:42 -0300

    improved permutations solution

--- src/backtracking/permutations.c
+++ src/backtracking/permutations.c
@@ -6,7 +6,7 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 20:02:10 by bmoreira          #+#    #+#             */
-/*   Updated: 2026/04/03 20:55:25 by bmoreira         ###   ########.fr       */
+/*   Updated: 2026/04/03 21:46:53 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,78 +24,85 @@ int	ft_strlen(char *str)
 	return (len);
 }
 
-char	*ft_strdup(char *str)
-{
-	char	*new;
-	int		i;
-
-	i = 0;
-	if (!str)
-		return (NULL);
-	new = calloc(ft_strlen(str) + 1, sizeof(char));
-	while (str[i])
-	{
-		new[i] = str[i];
-		i++;
-	}
-	return (new);
-}
-
-void	order_str(char *string, char *ascii)
-{
-	char	*new;
-	int		i;
-
-	i = 0;
-	while (string[i])
-	{
-		ascii[(int)string[i]] = 1;
-		i++;
-	}
-	i = 0;
-	new = ascii;
-	while (i < 256)
-	{
-		if (ascii[i])
-			*new++ = i;
-		i++;
-	}
-	*new = 0;
-}
-
-void	permutations(char *set, char *used, char *string, int pos)
+int	ft_strchr(char *str, char c)
 {
 	int	i;
 
-	i = -1;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_swap(char *i, char *j)
+{
+	char	aux;
+
+	aux = *i;
+	*i = *j;
+	*j = aux;
+}
+
+char	*sort_str(char *string)
+{
+	int	swapped;
+	int	len;
+	int	i;
+
+	len = ft_strlen(string);
+	swapped = 1;
+	while (swapped)
+	{
+		swapped = 0;
+		i = 0;
+		while (i < len - 1)
+		{
+			if (string[i] > string[i + 1])
+			{
+				ft_swap(&string[i], &string[i + 1]);
+				swapped = 1;
+			}
+			i++;
+		}
+	}
+	return (string);
+}
+
+void	permutations(char *set, char *string, int pos)
+{
+	int	i;
+
+	i = 0;
 	if (!set[pos])
 	{
 		puts(string);
 		return ;
 	}
-	while (set[++i])
+	while (set[i])
 	{
-		if (used[(int) set[i]])
-			continue ;
-		used[(int) set[i]] = 1;
-		string[pos] = set[i];
-		permutations(set, used, string, pos + 1);
-		used[(int) set[i]] = 0;
+		if (!ft_strchr(string, set[i]))
+		{
+			string[pos] = set[i];
+			permutations(set, string, pos + 1);
+			string[pos] = 0;
+		}
+		i++;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	char	ordered[256] = {0};
-	char	used[256] = {0};
 	char	*new;
-	char	*dup;
+	char	*sorted;
 
 	if (argc != 2)
 		return (1);
 	new = calloc(ft_strlen(argv[1]) + 1, sizeof(char));
-	dup = ft_strdup(argv[1]);
-	order_str(dup, ordered);
-	permutations(ordered, used, new, 0);
+	sorted = sort_str(argv[1]);
+	permutations(sorted, new, 0);
 	return (0);
 }
